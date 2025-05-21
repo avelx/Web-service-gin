ca045ac455ff414d17701c7f6202f50013cebafb
Pavel <pavel.oliynyk@gmail.com>
2025-05-21 12:43:58 +0100

    * dynamically read data from Csv file and feed via endpoint

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
 )
 
 // album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
-	Title  string  `json:"title"`
+	Title  string  `json:"Title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
+}
+
+type track struct {
+	TrackId string `json:"TrackId"`
+	AlbumId string `json:"AlbumId"`
+	Title   string `json:"Title"`
 }
 
 // albums slice to seed record album data.
@@ -20,11 +30,49 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+var allTracks = []track{
+	{TrackId: "1", AlbumId: "23", Title: "test"},
+	{TrackId: "2", AlbumId: "23", Title: "test"},
+	{TrackId: "3", AlbumId: "23", Title: "test"},
+}
+
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
+}
+
+func processCsvFile(records *[][]string) []track {
+	rec := *records
+	var recs = make([]track, len(rec))
+	for i := 0; i < len(rec); i++ {
+		//id, _ := strconv.Atoi(rec[i][0])
+		var r = track{
+			TrackId: rec[i][0],
+			AlbumId: rec[i][1],
+			Title:   rec[i][2],
+		}
+		recs[i] = r
+	}
+	return recs
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
+	router.GET("/tracks", getTracks)
 
 	router.Run("localhost:8080")
 }
@@ -34,16 +82,21 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getTracks(c *gin.Context) {
+	records := readCsvFile("data/tracks.csv")
+	recs := processCsvFile(&records)
+	fmt.Println("Record: ", recs)
+	c.IndentedJSON(http.StatusOK, recs)
+}
+
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
 	var newAlbum album
-
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
-
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
